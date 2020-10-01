7d1c6c6c60a91694371f5f6edc93815d13cca6e6
Ard2238 <abhishekrdas28@gmail.com>
2020-10-01 09:26:25 +0530

    Added Code to create a board

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,24 +1,18 @@
 package com.cg.tictactoe;
 
-public class TicTacToeGame {
+public class TicTacToeGame {	
 	
-	
-	public static void createBoard() {
-		
-		char[] board =  new char[10];
-		
+	//Creating a Board
+	public static char[] createBoard() {		
+		char[] board =  new char[10];		
 		for(int i=0; i<10; i++) {
 			board[i] = ' ';
 		}
+		return board;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Welcome to the TicTacToe Game");
-		
-		createBoard();
-
+	
+	public static void main(String[] args) {		
+		System.out.println("Welcome to the TicTacToe Game");		
+		char[] board = createBoard();
 	}
-
 }
