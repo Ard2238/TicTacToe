788714df7782c5242fb05728ecbb73b34e43eb56
Ard2238 <abhishekrdas28@gmail.com>
2020-10-01 11:28:25 +0530

    Modified Code to adjust errors in business logic

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TicTacToeGame {	
 	static Scanner sc = new Scanner(System.in);
+	static char playerLetter, computerLetter;
 	
 	/* UC1 -- Creating a Board */	 
 	public static char[] createBoard() {
@@ -16,29 +17,28 @@ public class TicTacToeGame {
 	
 	/* UC2 -- Taking Input */ 
 	public static char takeInput() {
-		System.out.println("Enter the input: ");
-		char input = sc.next().charAt(0);		
-		return input;		
+		System.out.println("Enter the input ('X' / 'O'): ");
+		char input = Character.toUpperCase(sc.next().charAt(0));		
+		if(input == 'X' || input == 'O')
+			return input;
+		else {
+			System.out.println("Invalid Character. Try Again.");
+			return takeInput();
+		}
 	}
 	
-	/* UC2 -- Deciding which player will proceed to play */
-	public static String decidePlayer() {
-		String player;
-		int temp = (int) (Math.random()*2 % 2);
-		if(temp == 0) {
-			player = "Player";
-		}else {
-			player = "Computer";
-		}
-		return player;
+	/* UC2 -- Determine letter for player and computer */
+	public static void setLetter(char playerLetter) {
+		computerLetter = (playerLetter == 'X')? 'O':'X';			
+		System.out.println("Player Letter: " + playerLetter + "\nComputer Letter : " + computerLetter);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TicTacToe Game");
 		char[] board = createBoard();
-		char input = takeInput();
-		String player = decidePlayer();
+		char playerLetter = takeInput();	
+		setLetter(playerLetter);
 		
-		System.out.println(player + " will begin now with " + input);
+		sc.close();
 	}
 }
